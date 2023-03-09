e6f6af27b64d9b6ff29edd977ca5f5837885eb79
ariel-adler <33860360+ariel-adler@users.noreply.github.com>
2023-03-09 13:24:58 +0200

    Add files via upload
    initiate

---
+++ src/main/java/com/tikalk/kafkagraphql/KafkaGraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.tikalk.kafkagraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaGraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaGraphqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tikalk/kafkagraphql/KafkaGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tikalk.kafkagraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
